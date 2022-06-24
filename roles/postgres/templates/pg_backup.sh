dc2b13f2faae05807440801a8e410adbb66c4933
Prabhanshu Gupta <guptaprabhanshu1@gmail.com>
2022-06-24 12:52:23 +0530

    Upgrade Postgres to 14 and Ubuntu to 20 (#15)
    Co-authored-by: vkrm <mail@vkrm.is>

--- roles/postgres/templates/pg_backup.sh
+++ roles/postgres/templates/pg_backup.sh
@@ -10,8 +10,8 @@ if ! mkdir -p $BACKUP_DIR; then
     exit 1;
 fi;
 
-if ! pg_dump -U {{ secrets.postgres.username }} {{ postgres.database_name }} | gzip > "$FILENAME".sql.gz.in_progress; then
-    echo "[!!ERROR!!] Failed to produce backup database {{ postgres.database_name }}" 1>&2
+if ! pg_dump -U {{ database.username }} {{ database_name }} | gzip > "$FILENAME".sql.gz.in_progress; then
+    echo "[!!ERROR!!] Failed to produce backup database {{ database_name }}" 1>&2
 else
     mv "$FILENAME".sql.gz.in_progress "$FILENAME".sql.gz
   {% for server in groups.storage %}
